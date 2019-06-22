880262eaf63e8f9210e2ce2f798a2cbfc993ea87
xuhuawei <xuhw@knowbox.cn>
2019-06-22 18:23:43 +0800

    fix

--- app/src/main/java/com/wzy/nesteddetail/activity/MainActivity.java
+++ app/src/main/java/com/wzy/nesteddetail/activity/MainActivity.java
@@ -1,80 +1,69 @@
 package com.wzy.nesteddetail.activity;
 
-import android.databinding.DataBindingUtil;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.wzy.nesteddetail.R;
 import com.wzy.nesteddetail.adapter.RvAdapter;
-import com.wzy.nesteddetail.databinding.ActivityMainBinding;
 import com.wzy.nesteddetail.model.InfoBean;
+import com.wzy.nesteddetail.view.NestedScrollingDetailContainer;
+import com.wzy.nesteddetail.view.NestedScrollingWebView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding mainBinding;
+    private TextView vToolBar;
+    private NestedScrollingDetailContainer nestedContainer;
 
+    private NestedScrollingWebView webContainer;
+    private RecyclerView rvList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        initImmersed();
+       setContentView(R.layout.activity_main );
         initView();
     }
-
-    private void initImmersed() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            View decorView = getWindow().getDecorView();
-            int option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-            decorView.setSystemUiVisibility(option);
-            getWindow().setNavigationBarColor(Color.TRANSPARENT);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-            ActionBar actionBar = getSupportActionBar();
-            actionBar.hide();
-        }
-    }
+   
 
     private void initView() {
+        vToolBar=findViewById(R.id.v_tool_bar);
+        nestedContainer=findViewById(R.id.nested_container);
+        webContainer=findViewById(R.id.web_container);
+        rvList=findViewById(R.id.rv_list);
         initWebView();
         initRecyclerView();
         initToolBarView();
     }
 
     private void initToolBarView() {
-        mainBinding.vToolBar.setOnClickListener(new View.OnClickListener() {
+         vToolBar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mainBinding.nestedContainer.scrollToTarget(mainBinding.rvList);
+                 nestedContainer.scrollToTarget(rvList);
             }
         });
     }
 
     private void initWebView() {
-        mainBinding.webContainer.getSettings().setJavaScriptEnabled(true);
-        mainBinding.webContainer.setWebViewClient(new WebViewClient());
-        mainBinding.webContainer.setWebChromeClient(new WebChromeClient());
-        mainBinding.webContainer.loadUrl("https://github.com/wangzhengyi/Android-NestedDetail");
+         webContainer.getSettings().setJavaScriptEnabled(true);
+         webContainer.setWebViewClient(new WebViewClient());
+         webContainer.setWebChromeClient(new WebChromeClient());
+         webContainer.loadUrl("https://github.com/wangzhengyi/Android-NestedDetail");
     }
 
     private void initRecyclerView() {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        mainBinding.rvList.setLayoutManager(layoutManager);
+         rvList.setLayoutManager(layoutManager);
         List<InfoBean> data = getCommentData();
         RvAdapter rvAdapter = new RvAdapter(this, data);
-        mainBinding.rvList.setAdapter(rvAdapter);
+         rvList.setAdapter(rvAdapter);
     }
 
     private List<InfoBean> getCommentData() {
